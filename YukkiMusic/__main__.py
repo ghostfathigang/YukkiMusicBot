dc0c99cdb3075db972fa379e254e64f832fa1793
ghostfathigang <105585539+ghostfathigang@users.noreply.github.com>
2022-09-17 11:48:41 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -62,7 +62,7 @@ async def init():
     await Yukki.start()
     try:
         await Yukki.stream_call(
-            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+            "https://telegra.ph/file/7249a3cf68e3028f9aec1.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("YukkiMusic").error(
